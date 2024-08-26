a1fb3eb40be467fde71b4e8f47f5ee7f7dff5b27
hondasatoshi0 <104481507+hondasatoshi0@users.noreply.github.com>
2024-08-26 13:40:27 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,12 +4,14 @@ import pandas as pd
 # アップローダー
 uploaded_file = st.file_uploader("アクセスログをアップロードしてください。")
 
+st.markdown('### 内容表記')
+
 # ファイル読み込み
 if uploaded_file:
   df = pd.read_csv(uploaded_file)
+  st.write(df.head(5))
 
-st.markdown('### 内容表記')
-st.write(df.head(5))
+
 
 # ページタイトルとページアイコン
 st.set_page_config(page_title="メインページ")
