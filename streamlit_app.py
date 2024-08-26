5d3dbc5c67b23e857e33b945163e08584f4b6ee5
hondasatoshi0 <104481507+hondasatoshi0@users.noreply.github.com>
2024-08-26 13:48:47 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,13 @@ if uploaded_file:
   df = pd.read_csv(uploaded_file)
   st.write(df.head(5))
 
+# 列選択のためのウィジェット
 usecols = st.multiselect(
   '何番目の列を解析の対象にしますか？',
   [0, 3, 4, 5, 6],
   [0, 3, 4, 5, 6]
 )
+
+# エラーメッセージ表示
+if len(usecols) == 0 or len(names) == 0:
+  st.error('解析対象の列が指定されていません。')
