4e37afc694590f4ef3e211a63dd6f773dcbd9cbf
hondasatoshi0 <104481507+hondasatoshi0@users.noreply.github.com>
2024-08-26 14:25:28 +0900

    Update streamlit_sample.py

--- streamlit_sample.py
+++ streamlit_sample.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 
+### 表示系 
 # テキスト（マークダウンで書ける）
 st.write("# tilte")
 
@@ -21,3 +22,39 @@ st.write(df)
 
 # チャート
 st.line_chart(df)
+
+### ユーザー入力系
+# テキスト入力
+name = st.text_input("名前")
+
+# 数値入力
+age = st.number_input("年齢",step = 1)
+
+st.write(f"名前:{name}")
+st.write(f"年齢:{age}")
+
+# ボタン
+if st.button("Push")
+  st.write("Button pushed.")
+
+# プルダウン
+select = st.selectbox("好きな役",["一発","立直","槍槓"])
+st.write(multi_select)
+
+# プルダウン（複数選択）
+multi_select = st.multiselect("好きな雀士",["多井","瀬戸熊","堀内","堀"])
+st.write(multi_select)
+
+# チェックボックス
+check = st.checkbox("OK")
+st.write(f"チェック:{check}")
+
+# ラジオボタン
+radio = st.radio("選択",["猫","犬"])
+st.write(f"ラジオ:{radio}")
+
+# ファイルアップロード
+uploaded_file = st.file_uploader("upload",type=["csv"])
+if uploaded_file:
+  df = pd.read_csv(uploaded_file)
+  st.write(df)
