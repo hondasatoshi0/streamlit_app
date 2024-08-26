284a1ce47f8c712bc790d2d5a6f6acec5c36ef9b
hondasatoshi0 <104481507+hondasatoshi0@users.noreply.github.com>
2024-08-26 17:08:50 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,28 +24,6 @@ usecols = st.multiselect(
   [0, 3, 4, 5, 6]
 )
 
-
-# ヘルプテキスト
-help_txt = '''
-        以下のフォーマット文字列を解析可能です。詳細については、[公式ページ](https://httpd.apache.org/docs/2.4/ja/mod/mod_log_config.html)を参照して下さい。
-
-        | 列名 | フォーマット文字列 | 説明 | 
-        |:-----|:-----:|:-----|
-        | Remote Host | `%h` | リモートホスト |
-        | Time | `%t` | リクエストを受付けた時刻 | 
-        | Request | `\"%r\"` | リクエストの最初の行 | 
-        | Status | `%>s` | ステータス | 
-        | Size | `%b` | レスポンスのバイト数 | 
-        | User Agent | `\"%{User-agent}i\"` | リクエストのUser-agentヘッダの内容 | 
-        | Response Time | `%D` または `%T` | リクエストを処理するのにかかった時間 |         
-        '''
-
-names = st.multiselect(
-        'これらの列を何を意味しますか？',
-        ['Remote Host', 'Time', 'Request', 'Status', 'Size', 'User Agent', 'Response Time'],
-        ['Remote Host', 'Time', 'Request', 'Status', 'Size', 'User Agent', 'Response Time'],
-        help=help_txt)
-
 # 世界地図の表示
 st.map()
 
