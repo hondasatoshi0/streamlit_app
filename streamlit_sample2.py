8de0a8564c9e88aa4e73cd3234085651f685d29b
hondasatoshi0 <104481507+hondasatoshi0@users.noreply.github.com>
2024-08-26 16:35:38 +0900

    Update streamlit_sample2.py

--- streamlit_sample2.py
+++ streamlit_sample2.py
@@ -2,5 +2,13 @@ import streamlit as st
 
 st.write("# Streamlit sample2")
 
+# 緯度経度データ（10進数）
+pref_list = [
+  {"longitude":130.741667, "latitude":32.789828}, # 熊本県
+  {"longitude":131.423855, "latitude":31.911090}, # 宮崎県
+  {"longitude":130.557981, "latitude":31.560148}, # 鹿児島県
+]
+
+
 # 世界地図の表示
-st.map()
+st.map(pref_list)
