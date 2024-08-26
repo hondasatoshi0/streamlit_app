6ffe93d565abc3a6fa7b47c5e3ed094a2e7cdb3d
hondasatoshi0 <104481507+hondasatoshi0@users.noreply.github.com>
2024-08-26 13:24:47 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,15 @@
 import streamlit as st
+import pandas as pd
+
+# アップローダー
 uploaded_file = st.file_uploader("アクセスログをアップロードしてください。")
+
+# ファイル読み込み
+if uploaded_file is not None:
+  df = pd.read_csv(
+    uploaded_file,
+    sep=r'\s(?=(?:[^"]*"[^"]*")*[^"]*$)(?![^\[]*\])',
+    engine='python',
+    na_values='-',
+    header=None
+  )
