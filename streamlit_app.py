09414aea27384e9fbe7a8147bca4493009ca719c
hondasatoshi0 <104481507+hondasatoshi0@users.noreply.github.com>
2024-08-26 13:37:36 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,5 +12,5 @@ st.markdown('### 内容表記')
 st.write(df.head(5))
 
 # ページタイトルとページアイコン
-st.set_page_config(page_title="メインページ",page_icon="icon.png")
+st.set_page_config(page_title="メインページ")
 st.title("Streamlit テストページ")
