9a18d7dbb32c5ce7eafd4e8d9d57a04713b62878
hondasatoshi0 <104481507+hondasatoshi0@users.noreply.github.com>
2024-08-26 14:12:20 +0900

    Update streamlit_sample.py

--- streamlit_sample.py
+++ streamlit_sample.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 # テキスト（マークダウンで書ける）
 st.write("# tilte")
@@ -8,3 +9,15 @@ st.caption("注釈")
 
 # 画像
 st.image("サンプル.png")
+
+# テーブル
+df = pd.DataFrame(
+  {
+    "first colum": [1,2,3,4],
+    "second column":[10,20,30,40],
+  }
+)
+st.write(df)
+
+# チャート
+st.line_chart(df)
