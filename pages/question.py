f29c1be379ebfbb68f2b1b125b574a29970554d4
hondasatoshi0 <104481507+hondasatoshi0@users.noreply.github.com>
2024-08-27 14:26:02 +0900

    Update question.py

--- pages/question.py
+++ pages/question.py
@@ -43,31 +43,31 @@ if st.session_state.page == 'page1':
     st.title("依頼書フォーム")
     
     # 部署
-    st.session_state.section = st.selectbox("所属部署", ["製造１課","製造２課","製造３課","エンジニアリング部","押出課","その他"], index = None, placeholder = "所属部署を選択してください。")
+    st.session_state.section = st.selectbox("所属部署*", ["製造１課","製造２課","製造３課","エンジニアリング部","押出課","その他"], index = None, placeholder = "所属部署を選択してください。")
     
     # 氏名
-    name = st.text_input("氏名")
+    name = st.text_input("氏名*")
     st.session_state.name = name.replace(' ','').replace('　','')
     st.write("氏名:",name)
     
     # 依頼内容
-    st.session_state.request = st.text_input("依頼内容（最大200文字）　※必須", max_chars = 200)
+    st.session_state.request = st.text_input("依頼内容（最大200文字）*", max_chars = 200)
     
     # ファイルアップロード
     st.write("写真や資料があればこちらからアップロードしてください。（１つまで）")
     st.session_state.uploaded_file = st.file_uploader("アップロード")
     
     # 依頼詳細１
-    st.session_state.request_detail1 = st.text_input("製品の品質に関する注意事項内容又は要望（最大200文字）　※必須", max_chars = 200)
+    st.session_state.request_detail1 = st.text_input("製品の品質に関する注意事項内容又は要望（最大200文字）*", max_chars = 200)
     
     # 依頼詳細２
-    st.session_state.request_detail2 = st.text_input("動作に関する注意事項又は要望（最大200文字）　※必須", max_chars = 200)
+    st.session_state.request_detail2 = st.text_input("動作に関する注意事項又は要望（最大200文字）*", max_chars = 200)
     
     # 依頼詳細３
-    st.session_state.request_detail3 = st.text_input("そのほかの注意事項内容又は要望（最大200文字）　※必須", max_chars = 200)
+    st.session_state.request_detail3 = st.text_input("そのほかの注意事項内容又は要望（最大200文字）*", max_chars = 200)
     
     # 希望納期
-    st.session_state.d = st.date_input("希望納期　※必須", value = None)
+    st.session_state.d = st.date_input("希望納期*", value = None)
     
     # 緊急性
     st.write("緊急ですか？")
@@ -75,8 +75,11 @@ if st.session_state.page == 'page1':
     
     # 送信ボタン
     if st.button("送信"):
-        with st.spinner('送信中...'):
-            time.sleep(2)
+        if st.session_state.request is not None\
+            and st.session_state.request
+
+
+        
         go_to_page('page2')
         st.rerun()
 
@@ -94,6 +97,7 @@ elif st.session_state.page == 'page2':
     
     if st.button("別の回答を送信"):
         go_to_page('page1')
+        st.rerun()
 
 
 
