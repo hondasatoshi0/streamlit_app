a059bf2b2a35992aa414799d3465b87ffa8b58af
hondasatoshi0 <104481507+hondasatoshi0@users.noreply.github.com>
2024-08-27 09:51:50 +0900

    Update question.py

--- pages/question.py
+++ pages/question.py
@@ -1,1 +1,12 @@
+import streamlit as st
+
+st.write("# 依頼書フォーム")
+
+# 部署
+section = st.selectbox("所属部署を選択してください。",["製造１課","製造２課","製造３課","エンジニアリング部","押出課","その他"])
+
+# 氏名
+st.write("氏名")
+st.caption("※空白を空けずに入力してください")
+name = st.text_input("氏名")
 
