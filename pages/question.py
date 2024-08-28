66e47d1077a27a90cc90247f4a216a3bc95036ac
satoshi honda <104481507+hondasatoshi0@users.noreply.github.com>
2024-08-28 17:43:42 +0900

    Update question.py

--- pages/question.py
+++ pages/question.py
@@ -162,16 +162,16 @@ elif st.session_state.page == 'page2':
         ]
 
         df = pd.DataFrame({
-            "依頼日時": datetime.datetime.now(pytz.timezone('Asia/Tokyo')).strftime("%Y/%m/%d %H:%M:%S"),
-            "所属部署": st.session_state.section,
-            "氏名": st.session_state.name,
-            "依頼内容": st.session_state.request,
-            "ファイル": st.session_state.uploaded_file,
-            "製品の品質に関する注意事項内容又は要望": st.session_state.request_detail1,
-            "動作に関する注意事項又は要望": st.session_state.request_detail2,
-            "そのほかの注意事項内容又は要望": st.session_state.request_detail3,
-            "希望納期": st.session_state.d.strftime("%Y/%m/%d"),
-            "緊急性": st.session_state.check
+            "依頼日時": [datetime.datetime.now(pytz.timezone('Asia/Tokyo')).strftime("%Y/%m/%d %H:%M:%S")],
+            "所属部署": [st.session_state.section],
+            "氏名": [st.session_state.name],
+            "依頼内容": [st.session_state.request],
+            "ファイル": [st.session_state.uploaded_file],
+            "製品の品質に関する注意事項内容又は要望": [st.session_state.request_detail1],
+            "動作に関する注意事項又は要望": [st.session_state.request_detail2],
+            "そのほかの注意事項内容又は要望": [st.session_state.request_detail3],
+            "希望納期": [st.session_state.d.strftime("%Y/%m/%d")],
+            "緊急性": [st.session_state.check],
         })
 
         st.write(df)
