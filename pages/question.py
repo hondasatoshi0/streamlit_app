09754be69c586e04eccd13b10b000bfe4f07f555
satoshi honda <104481507+hondasatoshi0@users.noreply.github.com>
2024-08-28 14:14:49 +0900

    Update question.py

--- pages/question.py
+++ pages/question.py
@@ -7,7 +7,7 @@ import re
 # vsc 追加
 # 接続文字列を設定
 conn_str = (
-    'DRIVER={SQL Server};'
+    'DRIVER={Microsoft JDBC Driver 9.2 for SQL Server};'
     'SERVER=192.168.1.5\SQLEXPRESS;'
     'DATABASE=IoTtest;'
     'UID=sa;'
