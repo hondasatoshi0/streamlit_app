4cef23addb351d905dd6a79bf5d2723c4250f112
satoshi honda <104481507+hondasatoshi0@users.noreply.github.com>
2024-08-28 18:05:15 +0900

    Update 依頼書.py

--- pages/依頼書.py
+++ pages/依頼書.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.write("# 依頼書")
 
-st.page_link("pages/依頼書/requestForm.py", label="依頼書フォーム",icon="🌟")
-st.page_link("pages/依頼書/requestData.py", label="依頼一覧",icon="🌟")
+st.page_link("依頼書/requestForm.py", label="依頼書フォーム",icon="🌟")
+st.page_link("依頼書/requestData.py", label="依頼一覧",icon="🌟")
