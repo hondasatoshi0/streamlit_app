72e74eb4a71e2fdfa332293621712c959efc8fc2
satoshi honda <104481507+hondasatoshi0@users.noreply.github.com>
2024-08-28 16:55:06 +0900

    Update question.py

--- pages/question.py
+++ pages/question.py
@@ -47,7 +47,7 @@ if st.session_state.page == 'page1':
     st.caption("*必須の質問")
 
     # 現在日時
-    st.write(f"{datetime.datetime.now()}")
+    st.write(f"{datetime.datetime.now().format("%Y/%M/%D %h:%m:%s")}")
     
     
     # 部署
