647de76a47f83129d3d86cb75ec0d09560500070
satoshi honda <104481507+hondasatoshi0@users.noreply.github.com>
2024-08-28 15:57:22 +0900

    Update gsheets_test.py

--- pages/gsheets_test.py
+++ pages/gsheets_test.py
@@ -3,11 +3,11 @@ from google.oauth2 import service_account
 from gsheetsdb import connect
 import pandas as pd
 # Create a connection object.
-credentials = service_account.Credentials.from_service_account_file( '***.json', scopes=[ "https://www.googleapis.com/auth/spreadsheets", ],
+credentials = service_account.Credentials.from_service_account_info( st.secrets["gcp_service_account"], scopes=[ "https://www.googleapis.com/auth/spreadsheets", ],
 )
 conn = connect(credentials=credentials)
 def run_query(query): rows = conn.execute(query, headers=1) return rows
-sheet_url = "https://docs.google.com/spreadsheets/d/15-5s3LOdSheVRPsrhhfeYdfC-QLi7TQVXhYdwbSu-oc/edit?usp=sharing"
+sheet_url = st.secrets["private_gsheets_url"]
 rows = run_query(f'SELECT * FROM "{sheet_url}"')
 # データフレームに変換し表示する
 row_list = []
