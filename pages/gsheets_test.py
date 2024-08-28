2ba02944533f4acd51572602d693139e2485620b
satoshi honda <104481507+hondasatoshi0@users.noreply.github.com>
2024-08-28 15:56:22 +0900

    Update gsheets_test.py

--- pages/gsheets_test.py
+++ pages/gsheets_test.py
@@ -1,19 +1,16 @@
 import streamlit as st
+from google.oauth2 import service_account
 from gsheetsdb import connect
 import pandas as pd
-
-conn = connect()
-def run_query(query):
-  rows = conn.execute(query, headers=1)
-  return rows
-
+# Create a connection object.
+credentials = service_account.Credentials.from_service_account_file( '***.json', scopes=[ "https://www.googleapis.com/auth/spreadsheets", ],
+)
+conn = connect(credentials=credentials)
+def run_query(query): rows = conn.execute(query, headers=1) return rows
 sheet_url = "https://docs.google.com/spreadsheets/d/15-5s3LOdSheVRPsrhhfeYdfC-QLi7TQVXhYdwbSu-oc/edit?usp=sharing"
-
 rows = run_query(f'SELECT * FROM "{sheet_url}"')
-
-st.write("スプレッドシート内容表示")
+# データフレームに変換し表示する
 row_list = []
-for row in rows:
-  row_list.append(row)
-  df=pd.DataFrame(row_list)
-  st.table(df)
+for row in rows: row_list.append(row)
+df=pd.DataFrame(row_list)
+st.table(df)
