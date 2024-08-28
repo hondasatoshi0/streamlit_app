dcdaed511f5dcd09d194789ee3f4a91154ff0c16
satoshi honda <104481507+hondasatoshi0@users.noreply.github.com>
2024-08-28 17:57:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ st.write("### サンプル")
 st.page_link("pages/streamlit_sample.py", label="sample 1",icon="1⃣")
 st.page_link("pages/streamlit_sample2.py", label="sample 2",icon="2⃣")
 st.page_link("pages/requestForm.py", label="依頼書フォーム",icon="🌟")
-st.page_link("pages/requestData.py", label="依頼一覧",icon="🌟)
+st.page_link("pages/requestData.py", label="依頼一覧",icon="🌟")
 
 # アップローダー
 uploaded_file = st.file_uploader("アクセスログをアップロードしてください。")
