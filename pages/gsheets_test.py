d3dc6059cfc6d94e0297429a6bb8189ae9f6a9a3
satoshi honda <104481507+hondasatoshi0@users.noreply.github.com>
2024-08-28 15:49:29 +0900

    Update gsheets_test.py

--- pages/gsheets_test.py
+++ pages/gsheets_test.py
@@ -1,5 +1,19 @@
 import shillelagh
 import gsheetsdb
+import pandas as pd
 
+conn = connect()
+def run_query(query):
+  rows = conn.execute(query, headers=1)
+  return rows
 
-https://docs.google.com/spreadsheets/d/15-5s3LOdSheVRPsrhhfeYdfC-QLi7TQVXhYdwbSu-oc/edit?usp=sharing
+sheet_url = "https://docs.google.com/spreadsheets/d/15-5s3LOdSheVRPsrhhfeYdfC-QLi7TQVXhYdwbSu-oc/edit?usp=sharing"
+
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
+
+st.write("スプレッドシート内容表示")
+row_list = []
+for row in rows:
+  row_list.append(row)
+  df=pd.DataFrame(row_list)
+  st.table(df)
