94e7961caa993831c535f30ee7dd36436a3229e8
satoshi honda <104481507+hondasatoshi0@users.noreply.github.com>
2024-08-28 16:08:00 +0900

    Update gsheets_test.py

--- pages/gsheets_test.py
+++ pages/gsheets_test.py
@@ -1,18 +1,20 @@
 import streamlit as st
-from google.oauth2 import service_account
-from gsheetsdb import connect
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
-# Create a connection object.
-credentials = service_account.Credentials.from_service_account_info( st.secrets["gcp_service_account"], scopes=[ "https://www.googleapis.com/auth/drive", ],
-)
-conn = connect(credentials=credentials)
-def run_query(query):
-  rows = conn.execute(query, headers=1) 
-  return rows
-sheet_url = st.secrets["private_gsheets_url"]
-rows = run_query(f'SELECT * FROM "{sheet_url}"')
-# データフレームに変換し表示する
-row_list = []
-for row in rows: row_list.append(row)
-df=pd.DataFrame(row_list)
-st.table(df)
+
+# Google Sheets APIにアクセスするための認証情報を設定
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_name("path_to_your_json_file.json", scope)
+client = gspread.authorize(creds)
+
+# スプレッドシートにアクセス
+spreadsheet = client.open("Your Spreadsheet Name")  # スプレッドシートの名前を指定
+worksheet = spreadsheet.get_worksheet(0)  # 最初のワークシートを取得
+
+# データを取得してDataFrameに変換
+data = worksheet.get_all_records()
+df = pd.DataFrame(data)
+
+# Streamlitでデータを表示
+st.write(df)
