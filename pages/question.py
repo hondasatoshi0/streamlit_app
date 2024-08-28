4c607afc136b786254fad08b594a58cf464532c8
satoshi honda <104481507+hondasatoshi0@users.noreply.github.com>
2024-08-28 15:05:47 +0900

    Update question.py

--- pages/question.py
+++ pages/question.py
@@ -3,6 +3,22 @@ import datetime
 import time
 import re
 
+# vsc 追加
+# 接続文字列を設定    
+conn_str =　("DRIVER={" + f"{st.secrets["DRIVER"]}" + f"};SERVER={st.secrets["SERVER"]};uid={st.secrets["UID"]};pwd={st.secrets["PWD"]};DATABASE={st.secrets["DATABASE"]};")
+# 接続を作成
+conn = pyodbc.connect(conn_str)
+# カーソルを作成
+cursor = conn.cursor()
+# クエリを実行
+cursor.execute('SELECT * FROM T_設備機械課員')
+# 結果を取得
+rows = cursor.fetchall()
+for row in rows:
+    st.write(f"{row}")
+# 接続を閉じる
+conn.close()
+
 # 初期値設定
 if 'section' not in st.session_state:
     st.session_state.section = ''
