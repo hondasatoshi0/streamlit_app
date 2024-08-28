9bb93a83f659bfd268a1d2be8cad9c22711811b3
hondasatoshi0 <104481507+hondasatoshi0@users.noreply.github.com>
2024-08-29 08:59:43 +0900

    移行

--- main.py
+++ main.py
@@ -10,23 +10,3 @@ st.page_link("pages/streamlit_sample.py", label="sample 1",icon="1⃣")
 st.page_link("pages/streamlit_sample2.py", label="sample 2",icon="2⃣")
 st.page_link("pages/requestForm.py", label="依頼書フォーム",icon="🌟")
 st.page_link("pages/requestData.py", label="依頼一覧",icon="🌟")
-
-# アップローダー
-uploaded_file = st.file_uploader("アクセスログをアップロードしてください。")
-
-st.markdown('### 内容表記')
-
-# ファイル読み込み
-if uploaded_file:
-  df = pd.read_csv(uploaded_file)
-  st.write(df.head(5))
-
-# 列選択のためのウィジェット
-usecols = st.multiselect(
-  '何番目の列を解析の対象にしますか？',
-  [0, 3, 4, 5, 6],
-  [0, 3, 4, 5, 6]
-)
-
-# 世界地図の表示
-st.map()
--- pages/streamlit_sample.py
+++ pages/streamlit_sample.py
@@ -56,11 +56,21 @@ st.write(f"チェック:{check}")
 radio = st.radio("選択",["猫","犬"])
 st.write(f"ラジオ:{radio}")
 
-# ファイルアップロード
-uploaded_file = st.file_uploader("upload",type=["csv"])
+# 列選択のためのウィジェット
+usecols = st.multiselect(
+  '何番目の列を解析の対象にしますか？',
+  [0, 3, 4, 5, 6],
+  [0, 3, 4, 5, 6]
+)
+
+# アップローダー
+uploaded_file = st.file_uploader("アクセスログをアップロードしてください。")
+st.markdown('### 内容表記')
+# ファイル読み込み
 if uploaded_file:
   df = pd.read_csv(uploaded_file)
-  st.write(df)
+  st.write(df.head(5))
+
 
 ### レイアウト系
 # 横に並べる
