5d7a212e719876e97b5318254f581808ee4abbf3
hondasatoshi0 <104481507+hondasatoshi0@users.noreply.github.com>
2024-08-29 17:41:04 +0900

    adsasda

--- pages/EXbets.py
+++ pages/EXbets.py
@@ -70,39 +70,40 @@ try:
                     category_list
                 )
 
-                credentials = {
-                    "type": st.secrets["gcp_service_account"]["type"],
-                    "project_id": st.secrets["gcp_service_account"]["project_id"],
-                    "private_key_id": st.secrets["gcp_service_account"]["private_key_id"],
-                    "private_key": st.secrets["gcp_service_account"]["private_key"],
-                    "client_email": st.secrets["gcp_service_account"]["client_email"],
-                    "client_id": st.secrets["gcp_service_account"]["client_id"],
-                    "auth_uri": st.secrets["gcp_service_account"]["auth_uri"],
-                    "token_uri": st.secrets["gcp_service_account"]["token_uri"],
-                    "auth_provider_x509_cert_url": st.secrets["gcp_service_account"]["auth_provider_x509_cert_url"],
-                    "client_x509_cert_url": st.secrets["gcp_service_account"]["client_x509_cert_url"]
-                }
+                if st.button("取得"):
+                    credentials = {
+                        "type": st.secrets["gcp_service_account"]["type"],
+                        "project_id": st.secrets["gcp_service_account"]["project_id"],
+                        "private_key_id": st.secrets["gcp_service_account"]["private_key_id"],
+                        "private_key": st.secrets["gcp_service_account"]["private_key"],
+                        "client_email": st.secrets["gcp_service_account"]["client_email"],
+                        "client_id": st.secrets["gcp_service_account"]["client_id"],
+                        "auth_uri": st.secrets["gcp_service_account"]["auth_uri"],
+                        "token_uri": st.secrets["gcp_service_account"]["token_uri"],
+                        "auth_provider_x509_cert_url": st.secrets["gcp_service_account"]["auth_provider_x509_cert_url"],
+                        "client_x509_cert_url": st.secrets["gcp_service_account"]["client_x509_cert_url"]
+                    }
 
-                # 認証情報を使用してGoogle Sheets APIにアクセス
-                scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-                creds = ServiceAccountCredentials.from_json_keyfile_dict(credentials, scope)
-                client = gspread.authorize(creds)
+                    # 認証情報を使用してGoogle Sheets APIにアクセス
+                    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+                    creds = ServiceAccountCredentials.from_json_keyfile_dict(credentials, scope)
+                    client = gspread.authorize(creds)
 
-                # スプレッドシートにアクセス
-                spreadsheet = client.open_by_url("https://docs.google.com/spreadsheets/d/1r-g-Khzwjcc-SS0CRtXWj5Ljx4u3YAfRRUR-LjRtwbA/edit?usp=sharing")  # スプレッドシートの名前を指定
-                worksheet = spreadsheet.get_worksheet(0)  # 最初のワークシートを取得
+                    # スプレッドシートにアクセス
+                    spreadsheet = client.open_by_url("https://docs.google.com/spreadsheets/d/1r-g-Khzwjcc-SS0CRtXWj5Ljx4u3YAfRRUR-LjRtwbA/edit?usp=sharing")  # スプレッドシートの名前を指定
+                    worksheet = spreadsheet.get_worksheet(0)  # 最初のワークシートを取得
 
-                # データを取得してDataFrameに変換
-                data = worksheet.get_all_records()
+                    # データを取得してDataFrameに変換
+                    data = worksheet.get_all_records()
 
-                # 抽出実行「所属部署」
-                data_filter = list(filter(lambda x : x["カテゴリー"] in category_select, data))
+                    # 抽出実行「所属部署」
+                    data_filter = list(filter(lambda x : x["カテゴリー"] in category_select, data))
 
-                df = pd.DataFrame(data_filter)
+                    df = pd.DataFrame(data_filter)
 
-                # Streamlitでデータを表示
-                st.write("### ギャンブル収支表")
-                st.write(df)
+                    # Streamlitでデータを表示
+                    st.write("### ギャンブル収支表")
+                    st.write(df)
 
                 st.markdown("---") # 区切り線
 
