f44389831dda69edc719f03944338a1a3fac63fb
hondasatoshi0 <104481507+hondasatoshi0@users.noreply.github.com>
2024-08-29 10:25:57 +0900

    aa

--- main.py
+++ main.py
@@ -46,7 +46,6 @@ else:
     # ページリンク
     st.write("### サンプル")
 
-    st.page_link("main.py", label="ホーム",icon="🏠")
     st.page_link("pages/streamlit_sample.py", label="sample 1",icon="1⃣")
     st.page_link("pages/streamlit_sample2.py", label="sample 2",icon="2⃣")
     st.page_link("pages/requestForm.py", label="依頼書フォーム",icon="🌟")
