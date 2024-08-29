f6a6db3c6318e6dfc8f1c2776731232bae681535
hondasatoshi0 <104481507+hondasatoshi0@users.noreply.github.com>
2024-08-29 11:19:05 +0900

    a

--- pages/streamlit_sample2.py
+++ pages/streamlit_sample2.py
@@ -12,27 +12,30 @@ st.markdown("""
 
 st.sidebar.page_link("main.py", label="ホーム",icon="🏠")
 
-if 'authenticated' not in st.session_state and not st.session_state['authenticated']:
+if 'authenticated' in st.session_state:
+  if st.session_state['authenticated']:
+    st.write("# Streamlit sample2")
+
+    # 緯度経度データ（10進数）
+    pref_list = [
+      {"longitude":135.161937, "latitude":33.894175}, # 本社
+      {"longitude":135.139814, "latitude":33.89986}, # 美浜
+      {"longitude":135.512259, "latitude":34.6832}, # 大阪支店
+      {"longitude":139.757387,"latitude":35.666272}, # 東京営業所
+      {"longitude":136.320687,"latitude":36.3034511}, # 北陸営業所
+    ]
+
+    # 世界地図の表示
+    st.map(pref_list,size=1)
+
+
+    # プログレスバー
+    my_bar = st.progress(0)
+    for i in range(0,100):
+      time.sleep(0.1)
+      my_bar.progress(i+1)
+
+  else:
     st.page_link("main.py","ログインページへ",icon="🏠")
 else:
-  st.write("# Streamlit sample2")
-
-  # 緯度経度データ（10進数）
-  pref_list = [
-    {"longitude":135.161937, "latitude":33.894175}, # 本社
-    {"longitude":135.139814, "latitude":33.89986}, # 美浜
-    {"longitude":135.512259, "latitude":34.6832}, # 大阪支店
-    {"longitude":139.757387,"latitude":35.666272}, # 東京営業所
-    {"longitude":136.320687,"latitude":36.3034511}, # 北陸営業所
-  ]
-
-  # 世界地図の表示
-  st.map(pref_list,size=1)
-
-
-  # プログレスバー
-  my_bar = st.progress(0)
-  for i in range(0,100):
-    time.sleep(0.1)
-    my_bar.progress(i+1)
-
+  st.page_link("main.py","ログインページへ",icon="🏠")
