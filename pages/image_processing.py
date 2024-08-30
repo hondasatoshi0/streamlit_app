668b4b08aa2cc10c922ae449ec40c3ea4c97b88d
hondasatoshi0 <104481507+hondasatoshi0@users.noreply.github.com>
2024-08-30 11:52:40 +0900

    ss

--- pages/image_processing.py
+++ pages/image_processing.py
@@ -1,15 +1,12 @@
 import streamlit as st
 import cv2
+import numpy as np
 
 st.title("画像処理")
 
 # アップローダー
-uploaded_file = st.file_uploader("画像をアップロードしてください。",type=["png","jpg","jpeg"])
+file_path = st.file_uploader("画像をアップロードしてください。",type=["png","jpg","jpeg"])
 
-st.write(uploaded_file)
+image = cv2.imread(file_path.name)
 
-image = cv2.imread(uploaded_file.name)
-
-cv2.imshow("Normal",image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+st.image(image)
