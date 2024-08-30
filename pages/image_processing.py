7d1eebbff6d8ec3baee68c7e5308ff35c0cf1792
hondasatoshi0 <104481507+hondasatoshi0@users.noreply.github.com>
2024-08-30 11:49:20 +0900

    sa

--- pages/image_processing.py
+++ pages/image_processing.py
@@ -4,11 +4,11 @@ import cv2
 st.title("画像処理")
 
 # アップローダー
-uploaded_file = st.file_uploader("アクセスログをアップロードしてください。",type="png")
+uploaded_file = st.file_uploader("画像をアップロードしてください。",type=["png","jpg","jpeg"])
 
 st.write(uploaded_file)
 
-image = cv2.imread(uploaded_file["name"])
+image = cv2.imread(uploaded_file.name)
 
 cv2.imshow("Normal",image)
 cv2.waitKey(0)
