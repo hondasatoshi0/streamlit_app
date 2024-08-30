36e9757e33d3c09485e50def9a248e10c1414d94
hondasatoshi0 <104481507+hondasatoshi0@users.noreply.github.com>
2024-08-30 14:25:39 +0900

    ra

--- pages/image_processing.py
+++ pages/image_processing.py
@@ -8,36 +8,35 @@ st.title("画像処理")
 # 画像をアップロード
 uploaded_file = st.file_uploader("画像をアップロード", type=["jpg", "png", "jpeg"])
 
-if st.button("実行"):
-    if uploaded_file is not None:
-        # PILで画像を読み込む
-        image = Image.open(uploaded_file)
+if uploaded_file is not None:
+    # PILで画像を読み込む
+    image = Image.open(uploaded_file)
 
-        # 画像をOpenCVの形式に変換
-        image_np = np.array(image)
-        # グレースケールに変換
-        gray_image = cv2.cvtColor(image_np, cv2.COLOR_BGR2GRAY)
-        # エッジ
-        edges_image = cv2.Canny(gray_image, 100 ,200)
-        # コーナー
-        img_dst = np.copy(gray_image)
-        dst = cv2.cornerHarris(gray_image, 2, 3, 0.04, img_dst)
-        dst = cv2.dilate(dst,None,iterations=3)
+    # 画像をOpenCVの形式に変換
+    image_np = np.array(image)
+    # グレースケールに変換
+    gray_image = cv2.cvtColor(image_np, cv2.COLOR_BGR2GRAY)
+    # エッジ
+    edges_image = cv2.Canny(gray_image, 100 ,200)
+    # コーナー
+    img_dst = np.copy(gray_image)
+    dst = cv2.cornerHarris(gray_image, 2, 3, 0.04, img_dst)
+    dst = cv2.dilate(dst,None,iterations=3)
 
-        corners_image = np.copy(image_np)
-        corners_image[dst>0.05*dst.max()] = [0,0,255]
+    corners_image = np.copy(image_np)
+    corners_image[dst>0.05*dst.max()] = [0,0,255]
 
-        cols1 = st.columns(2)
-        with cols1[0]:
-            st.image(image_np, caption="リサイズされた画像", use_column_width=True)
-        with cols1[1]:
-            st.image(gray_image, caption="グレー画像", use_column_width=True)
+    cols1 = st.columns(2)
+    with cols1[0]:
+        st.image(image_np, caption="リサイズされた画像", use_column_width=True)
+    with cols1[1]:
+        st.image(gray_image, caption="グレー画像", use_column_width=True)
 
 
-        cols2 = st.columns(2)
-        with cols2[0]:
-            st.image(edges_image, caption="エッジ画像", use_column_width=True)
-        with cols2[1]:
-            st.image(corners_image, caption="コーナー画像", use_column_width=True)
-    else:
-        st.error("実行できませんでした。画像をアップロードしてください。")
+    cols2 = st.columns(2)
+    with cols2[0]:
+        st.image(edges_image, caption="エッジ画像", use_column_width=True)
+    with cols2[1]:
+        st.image(corners_image, caption="コーナー画像", use_column_width=True)
+
+    uploaded_file = None
