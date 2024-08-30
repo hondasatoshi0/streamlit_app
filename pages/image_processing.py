bf2c821f1a4a5bb348d362a9cf17719137b1d3b3
hondasatoshi0 <104481507+hondasatoshi0@users.noreply.github.com>
2024-08-30 11:57:30 +0900

    as

--- pages/image_processing.py
+++ pages/image_processing.py
@@ -14,10 +14,5 @@ if uploaded_file is not None:
     image_np = np.array(image)
     image_cv = cv2.cvtColor(image_np, cv2.COLOR_RGB2BGR)
 
-    # 画像を表示（OpenCVウィンドウ）
-    cv2.imshow("Uploaded Image", image_cv)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
     # Streamlit上で画像を表示
     st.image(image, caption="アップロードされた画像", use_column_width=True)
