7ce1e49e30764147d828a11e54675f68ce860d5b
hondasatoshi0 <104481507+hondasatoshi0@users.noreply.github.com>
2024-09-10 14:25:07 +0900

    sa

--- pages/graph_plotting.py
+++ pages/graph_plotting.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 st.title = ("グラフ表示")
 # サイドバー設定
@@ -8,7 +9,7 @@ st.sidebar.page_link("main.py", label="ホーム",icon="🏠")
 st.sidebar.write("### 処理")
 st.sidebar.page_link("pages/image_processing.py",label="画像処理",icon="📷")
 st.sidebar.page_link("pages/character_processing.py",label="文字認識",icon="🔤")
-st.sidebar.page_link("pages/graph_.py",label="文字認識",icon="🔤")
+st.sidebar.page_link("pages/graph_plotting.py",label="グラフ表示",icon="📈")
 
 #
 st.write("# グラフ表示")
@@ -20,3 +21,9 @@ st.line_chart(chart_data)
 st.write('### グラフ２')
 # 画像をアップロード
 uploaded_file = st.file_uploader("テキスト形式ファイルをアップロード", type=["txt", "csv"])
+
+if uploaded_file is not None:
+    data_fream = pd.csv_reader(uploaded_file,header = 1, names=(("Date and time","実績ショット数","サイクルタイム実積値")))
+    st.line_chart(data_fream)
+
+
